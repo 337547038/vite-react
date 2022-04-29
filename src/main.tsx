a42660166184d70356309d73c0a78c0d119bb767
337547038 <337547038@qq.com>
2022-04-29 17:57:39 +0800

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter,BrowserRouter, Routes,Route } from 'react-router-dom'
-import App from './App'
+// import App from './App'
+import routerConfig from './router/index'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        {
+          routerConfig.routes.map((route) => {
+            return (
+              <Route key={route.path} {...route} />
+            )
+          })
+        }
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 )
