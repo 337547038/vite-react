7fefba3115b434990d343a0071148579d87ac717
337547038 <337547038@qq.com>
2022-04-29 17:57:39 +0800

    init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,19 @@
-import Home  from "../pages/index"
+/*import Home  from "../pages/index"
 export default [
   {
     exact: true,
     path: "/",
     component: Home
   }
-]
+]*/
+import BlogsList from '../pages/index'
+import BlogsDetail from '../pages/index'
+
+export default {
+  routes: [
+    { exact: true, path: '/', component: BlogsList },
+    { exact: true, path: '/blogs/detail/:article_id', component: BlogsDetail },
+  ],
+}
 /*
 https://github.com/JohnnyZhangQiao/lottery-box-react*/
