1990c9abf1ec57b2a50fad4ee06277bc31836a31
337547038 <337547038@qq.com>
2022-05-13 18:05:21 +0800

    input

--- src/packages/input/Input.tsx
+++ src/packages/input/Input.tsx
@@ -1,19 +1,19 @@
 import React,{forwardRef} from 'react'
 //import type { InputRef } from 'rc-input';
 interface Props{
-  value:any
+  value?:any
 }
 // HTMLInputElement
 interface InputRef{
   focus: (options?: any) => void;
   blur: () => void;
-  setSelectionRange: (start: number, end: number, direction?: 'forward' | 'backward' | 'none') => void;
-  select: () => void;
+  /*setSelectionRange: (start: number, end: number, direction?: 'forward' | 'backward' | 'none') => void;
+  select: () => void;*/
   input: HTMLInputElement | null;
 }
 const Input = forwardRef<InputRef, Props>((props, ref) => {
   return(
-    <input ref={ref}/>
+    <input ref={ref as React.Ref<HTMLInputElement>}/>
   )
 
 })
