918c54b07ee3b2b2e742f6b9be096bff8107bfde
337547038 <337547038@qq.com>
2022-05-26 11:29:41 +0800

    add loading

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,7 @@
 import React, {FC, useEffect, lazy} from 'react';
 import routes from './routesConfig';
 import {Route, Routes, useNavigate, Navigate, BrowserRouter} from 'react-router-dom';
+import {Loading} from '../packages/loading'
 // 路由装饰器
 const RouteDecorator = (props: { route: any }) => {
   const {route} = props;
@@ -20,7 +21,7 @@ const RouteDecorator = (props: { route: any }) => {
   return <route.component />;
 };
 const RouterComponent: FC = () => (
-  <React.Suspense fallback={<div>...</div>}>
+  <React.Suspense fallback={<Loading visible={true} size='small'/>}>
     <Routes>
       {
         routes.map(route => <Route key={route.path} path={route.path} element={<route.component />}>
